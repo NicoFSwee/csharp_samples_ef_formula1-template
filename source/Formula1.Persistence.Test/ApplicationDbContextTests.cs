ef348f9622f4f2ecba23197ab052e6bdae206bbc
NicoFSwee <56690947+NicoFSwee@users.noreply.github.com>
2020-03-05 23:03:18 +0100

    Add Dependencies, NuGet-Packages and more UnitTests

--- source/Formula1.Persistence.Test/ApplicationDbContextTests.cs
+++ source/Formula1.Persistence.Test/ApplicationDbContextTests.cs
@@ -5,44 +5,134 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Formula1.PersistenceTests
 {
-  [TestClass()]
-  public class ApplicationDbContextTests
-  {
-    private ApplicationDbContext GetDbContext(string dbName)
+    [TestClass()]
+    public class ApplicationDbContextTests
     {
-      // Build the ApplicationDbContext 
-      //  - with InMemory-DB
-      return new ApplicationDbContext(
-        new DbContextOptionsBuilder<ApplicationDbContext>()
-          .UseInMemoryDatabase(dbName)
-          .EnableSensitiveDataLogging()
-          .Options);
-    }
-
-    [TestMethod]
-    public async Task D01_FirstDataAccessTest()
-    {
-      string dbName = Guid.NewGuid().ToString();
-
-      using (ApplicationDbContext dbContext = GetDbContext(dbName))
-      {
-        Team team = new Team
+        private ApplicationDbContext GetDbContext(string dbName)
         {
-          Name = "Red Bull",
-          Nationality = "Austria"
-        };
-        dbContext.Teams.Add(team);
-        await dbContext.SaveChangesAsync();
-      }
-      using (ApplicationDbContext dbContext = GetDbContext(dbName))
-      {
-        var firstOrDefault = dbContext.Teams.FirstOrDefault(t => t.Name == "Red Bull");
-        Assert.IsNotNull(firstOrDefault, "Zumindest ein Team muss in DB sein");
-        Assert.AreEqual("Red Bull", firstOrDefault.Name);
-      }
+            // Build the ApplicationDbContext 
+            //  - with InMemory-DB
+            return new ApplicationDbContext(
+              new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(dbName)
+                .EnableSensitiveDataLogging()
+                .Options);
+        }
+
+        [TestMethod]
+        public async Task D01_FirstDataAccessTest()
+        {
+            string dbName = Guid.NewGuid().ToString();
+
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                Team team = new Team
+                {
+                    Name = "Red Bull",
+                    Nationality = "Austria"
+                };
+                dbContext.Teams.Add(team);
+                await dbContext.SaveChangesAsync();
+            }
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                var firstOrDefault = dbContext.Teams.FirstOrDefault(t => t.Name == "Red Bull");
+                Assert.IsNotNull(firstOrDefault, "Zumindest ein Team muss in DB sein");
+                Assert.AreEqual("Red Bull", firstOrDefault.Name);
+            }
+        }
+
+        [TestMethod]
+        public async Task D02_NationalityTest()
+        {
+            string dbName = Guid.NewGuid().ToString();
+
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                Team team1 = new Team
+                {
+                    Name = "Red Bull",
+                    Nationality = "Austria"
+                };
+                Team team2 = new Team
+                {
+                    Name = "Ferrari",
+                    Nationality = "Italy"
+                };
+                dbContext.Teams.Add(team1);
+                dbContext.Teams.Add(team2);
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                Team team = dbContext.Teams.FirstOrDefault(t => t.Nationality == "Italy");
+                Assert.IsNotNull(team, "Zumindest ein Team muss in DB sein");
+                Assert.AreEqual("Italy", team.Nationality);
+            }
+        }
+
+        [TestMethod]
+        public async Task D03_InsertDriverWithLinkedPropertie()
+        {
+            string dbName = Guid.NewGuid().ToString();
+
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                Team team1 = new Team
+                {
+                    Name = "Red Bull",
+                    Nationality = "Austria"
+                };
+                Team team2 = new Team
+                {
+                    Name = "Ferrari",
+                    Nationality = "Italy"
+                };
+                Driver driver1 = new Driver
+                {
+                    FirstName = "Hannes",
+                    LastName = "Bauer",
+                    Nationality = "Austria"
+                };
+                Driver driver2 = new Driver
+                {
+                    FirstName = "Giovanni",
+                    LastName = "Giotto",
+                    Nationality = "Italy"
+                };
+                Result result = new Result
+                {
+                    Race = new Race
+                    {
+                        Country = "Austria",
+                        City = "Salzburg",
+                        Date = DateTime.Now,
+                    },
+                    Team = team1,
+                    Driver = driver1,
+                    Position = 3,
+                    Points = 20
+                };
+
+                dbContext.Teams.Add(team2);
+                dbContext.Drivers.Add(driver2);
+                dbContext.Results.Add(result);
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (ApplicationDbContext dbContext = GetDbContext(dbName))
+            {
+                Driver driver = dbContext.Drivers.FirstOrDefault(d => d.FirstName == "Hannes");
+                Assert.IsNotNull(driver, "Zumindest ein Driver muss in DB sein");
+                Assert.AreEqual("Hannes", driver.FirstName);
+            }
+        }
+
     }
-  }
 }
